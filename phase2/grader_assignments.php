4cb9adb00a736246661aa206447ec0221c33142f
emileeautumn24 <229611179+emileeautumn24@users.noreply.github.com>
2026-03-23 16:51:11 -0400

    add "to-do" for grader/TA assignment php

--- phase2/grader_assignments.php
+++ phase2/grader_assignments.php
@@ -3,11 +3,8 @@
 $grader_id = $_POST["grader_id"];
 $course_id = $_POST["course_id"];
 $section_id = $_POST["section_id"];
-$semester_name = $_POST["semester_name"];
-$year_num = $_POST["year_num"];
-$post_text = $_POST["post_text"];
-$student_delete_id = $_POST["student_delete_id"];
-$type = $_POST["typeSelector"];  // Gets "STUDENT", "TA" or "GRADER"
+$semester_name = $_POST["semesterSelector"];
+$year_id = $_POST["year_id"];
 
 // checks connection
 $connection = mysqli_connect("localhost", "root", "")
@@ -15,6 +12,31 @@ or die ("Could not connect: " . mysqli_error($connection));
 
 // checks if database exists
 $db = mysqli_select_db($connection, "db2") or die ("Could not select database");
+
+// check if assign button was clicked
+if (isset($_POST["submit"])) {
+    // Submit Button was clicked
+
+    // check class section size in range [5, 10]
+    // for TA: check range [10, inf)
+    $query1 = "SELECT capacity FROM section WHERE section_id = " . $section_id . " AND course_id = " . course_id;
+    // if the result of the query is empty, invalid section or course id was entered
+    // tell user
+
+    // check if TA or grader is already TA/grading a section
+    $query2 = "";
+    // if TA/grader is already TA/grading a section
+    // tell user
+
+    // check eligibility
+    // TA: must be doctoral student
+    // grader: master/undergrad who got an A- or better in this course
+    $query3 = "";
+    // if ineligible, tell user
+
+    // if all conditions are met, assign that ID to be a TA/grader for that course
+
+}
 
 mysqli_close($connection);
 ?>
