f901a93b0b64d7ecef8326ad3ca994c8767d747c
emileeautumn24 <229611179+emileeautumn24@users.noreply.github.com>
2026-04-13 15:46:08 -0400

    add navigational ability to buttons

--- phase3/app/src/main/java/com/database2/android/studentinfo/mainMenuFragment.java
+++ phase3/app/src/main/java/com/database2/android/studentinfo/mainMenuFragment.java
@@ -3,17 +3,33 @@ package com.database2.android.studentinfo;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.database2.android.studentinfo.databinding.FragmentMainMenuBinding;
+
 public class mainMenuFragment extends Fragment {
+
+    private FragmentMainMenuBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_menu, container, false);
+        binding = FragmentMainMenuBinding.inflate(inflater, container, false);
+        binding.discussionButton.setOnClickListener(v -> NavHostFragment.findNavController(mainMenuFragment.this).navigate(R.id.action_mainMenuFragment_to_discussionFragment));
+        binding.evaluationButton.setOnClickListener(v -> NavHostFragment.findNavController(mainMenuFragment.this).navigate(R.id.action_mainMenuFragment_to_evaluationFragment));
+        binding.registerButton.setOnClickListener(v -> NavHostFragment.findNavController(mainMenuFragment.this).navigate(R.id.action_mainMenuFragment_to_registerFragment));
+        binding.transcriptButton.setOnClickListener(v -> NavHostFragment.findNavController(mainMenuFragment.this).navigate(R.id.action_mainMenuFragment_to_transcriptFragment));
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
